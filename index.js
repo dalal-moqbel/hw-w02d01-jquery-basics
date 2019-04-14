3c556e0797fdf70c1f290835c90077320fa2b93f
dalal-moqbel <dalal.alenzi1413@gmail.com>
2019-04-14 23:59:47 +0300

    done with homework

--- index.js
+++ index.js
@@ -1,24 +1,85 @@
 // turn on stop function 
-function turnOnStopLight() {
-    // change background color of stop light to red
-    // changes background color of green and yellow to black
-}
+$(document).ready(function(){
+  
+  
+  // change background color of stop light to red
+  // changes background color of green and yellow to black
 
+function turnOnStopLight(){
+$('#stoplignt').css('background-color','red');
+  $('#slowLight').css('background-color','black');
+   $('#goLight').css('background-color','black');
+}
+$(".bulb").on.click(turnOnStopLight(){
+               $("id").fadIn("slow");    
+                   }
 // turn on slow function
 function turnOnSlowLight() { 
-  // change background color of slow light to yellow
-  // changes background color of red and green to black
+ $('#slowLight').css('background-color','yellow')
+$('#stoplignt').css('background-color','black');
+$('#goLight').css('background-color','black');
+// change background color of slow light to yellow
+// changes background color of red and green to black
 }
 
 // turn on go function
-function turnOnGoLight() {
-    // change background color of go light to green
-    // changes background color of red and yellow to black
+function turnOnGoLight( ) {
+     $('goLight').css('background-color','green');
+     $('#stoplignt').css('background-color','black');
+     $('#slowLight').css('background-color','black');
+    // turn on stop function
+
+
+  // change background color of stop light to red
+  // changes background color of green and yellow to black
+
+
+
+function turnOnStopLight(){
+$('#stoplignt').css('background-color','red')
+  $('#slowLight').css('background-color','black');
+    $('#goLight').css('background-color','black');}
+
+// turn on slow function
+function turnOnSlowLight() { 
+ $('#slowLight').css('background-color','yellow')
+$('#stoplignt').css('background-color','black');
+$('#goLight').css('background-color','black');
+// change background color of slow light to yellow
+// changes background color of red and green to black
+}
+turnOnGoLight('green');
+// turn on go function
+function turnOnGoLight () {
+     $('goLight').css('background-color','green');
+     $('#stoplignt').css('background-color','black');
+     $('#slowLight').css('background-color','black');
+    
+  // change background color of go light to green
+  // changes background color of red and yellow to black
 }
 
 // set stop light click event
+$('stopButton').on('click',turnOnGoLight(){
+         $('.button').stop();
+
+                 });
 
 // set slow light click event
 
 // set go light click event
 
+  // change background color of go light to green
+  // changes background color of red and yellow to black
+}
+
+// set stop light click event
+
+
+// set slow light click event
+
+// set go light click event
+
+
+
+});
